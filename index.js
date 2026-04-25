af816a1723c1b088592e880c273699c0129c111e
lastfound <rafi.ibra09@gmail.com>
2026-04-25 19:05:55 +0700

    perbaikan file index.js const app

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ const groq = new OpenAI({
   baseURL: "https://api.groq.com/openai/v1", // Ini kuncinya!
 });
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://lastfound.github.io/portofoliio/', 'https://lastfound.github.io/', 'https://portofoliio-plum.vercel.app/'] // Sesuaikan dengan frontend kamu
-}));
+app.use(cors());
 app.use(express.json());
 
 app.post('/api/ai', async (req, res) => {
